1e1d2569094c0afb18b19defb5c5998300505734
Julie Burgoon <julieburgoon@Julies-MacBook-Air.local>
2021-06-29 16:15:22 -0600

    second

--- cards/app.js
+++ cards/app.js
@@ -3,22 +3,23 @@ let deck_id =''
 const img = document.createElement('img')
 const cards = document.querySelector('#cards')
 
+axios.get('https://deckofcardsapi.com/api/deck/new/draw/')
+.then(resp => console.log(resp.data.cards[0].value, resp.data.cards[0].suit))
 
-async function newCard(){
-const resp = await axios.get('https://deckofcardsapi.com/api/deck/new/draw/')
-console.log(resp.data.cards[0].value, resp.data.cards[0].suit, resp.data)
-}
-newCard()
 
-async function newDeck(){
-    const resp = await axios.get('https://deckofcardsapi.com/api/deck/new/shuffle/')
-    deck_id = resp.data.deck_id
-    console.log(resp)
-   
-}
-newDeck()
-async function nextCard(){
-    const card = await axios.get(`https://deckofcardsapi.com/api/deck/${deck_id}/draw/`)
+// async function newCard(){
+// const resp = await axios.get('https://deckofcardsapi.com/api/deck/new/draw/')
+// console.log(resp.data.cards[0].value, resp.data.cards[0].suit, resp.data)
+// }
+// newCard()
+
+
+axios.get('https://deckofcardsapi.com/api/deck/new/shuffle/')
+.then(resp => {
+    return deck_id = resp.data.deck_id
+})
+.then(deck_id => button.addEventListener('click',axios.get(`https://deckofcardsapi.com/api/deck/${deck_id}/draw/`)
+.then(card => {
     console.log(card)
     img.src = card.data.cards[0].image
     if(card.data.remaining >0){
@@ -27,6 +28,30 @@ async function nextCard(){
         cards.append('No more cards!!')
     }
     
-}
-nextCard()
-button.addEventListener('click', nextCard)
+})))
+
+// async function newDeck(){
+//     const resp = await axios.get('https://deckofcardsapi.com/api/deck/new/shuffle/')
+//     deck_id = resp.data.deck_id
+//     console.log(resp)
+   
+// }
+// newDeck()
+
+
+
+
+
+// async function nextCard(){
+//     const card = await axios.get(`https://deckofcardsapi.com/api/deck/${deck_id}/draw/`)
+//     console.log(card)
+//     img.src = card.data.cards[0].image
+//     if(card.data.remaining >0){
+//     cards.append(img)
+//     }else{
+//         cards.append('No more cards!!')
+//     }
+    
+// }
+// nextCard()
+// button.addEventListener('click', nextCard)
--- nums/app.js
+++ nums/app.js
@@ -5,27 +5,42 @@ const numList = document.querySelector('#multnumlist')
 const favList = document.querySelector('#multfactlist')
 
 const favNumarr = []
-async function favoriteNumber(){
-    const fav = await axios.get('http://numbersapi.com/15')
-    favNum.append(fav.data)
-}
-favoriteNumber()
+// async function favoriteNumber(){
+//     const fav = await axios.get('http://numbersapi.com/15')
+//     favNum.append(fav.data)
+// }
+// favoriteNumber()
+
+axios.get('http://numbersapi.com/15')
+.then(num => favNum.append(num.data))
 
 
-async function manyNums(){
-    const min = 1
-     const max = 10
-const nums = await axios.get(`http://numbersapi.com/${min}..${max}`)
-const numdata = nums.data
-  console.log(numdata)
-  for(i = min; i<=max; i++){
-       const li = document.createElement('li')
-   li.append(numdata[i])
-   numList.append(li)
-  }
-}
+// async function manyNums(){
+//     const min = 1
+//      const max = 10
+// const nums = await axios.get(`http://numbersapi.com/${min}..${max}`)
+// const numdata = nums.data
+//   console.log(numdata)
+//   for(i = min; i<=max; i++){
+//        const li = document.createElement('li')
+//    li.append(numdata[i])
+//    numList.append(li)
+//   }
+// }
     
-manyNums()
+// manyNums()
+
+const min = 1
+const max = 10
+axios.get(`http://numbersapi.com/${min}..${max}`)
+.then(nums =>{
+const numdata = nums.data
+console.log(numdata)
+for(i = min; i<=max; i++){
+     const li = document.createElement('li')
+ li.append(numdata[i])
+ numList.append(li)
+}})
 
 
 
@@ -33,17 +48,39 @@ manyNums()
 
 
 
-for(let i = 0; i<4; i++){
-    favNumarr.push(axios.get('http://numbersapi.com/15'))
-    console.log(favNumarr)
+// for(let i = 0; i<4; i++){
+//     favNumarr.push(axios.get('http://numbersapi.com/15'))
+//     console.log(favNumarr)
+// }
+
+
+// Promise.all(favNumarr)
+//     .then(res => (
+//         res.forEach(num =>{
+//         const li = document.createElement('li')
+//         li.append(num.data)
+//         favList.append(li)
+//         }
+//         )))
+
+
+async function favNums(){
+    let p1Promise = axios.get('http://numbersapi.com/15')
+    let p2Promise = axios.get('http://numbersapi.com/15')
+    let p3Promise = axios.get('http://numbersapi.com/15')
+    let p4Promise = axios.get('http://numbersapi.com/15')
+
+    let p1 = await p1Promise
+    let p2 = await p2Promise
+    let p3 = await p3Promise
+    let p4 = await p4Promise
+    const br = document.createElement('br')
+        favList.append(p1.data)
+        favList.append(p2.data)
+        favList.append(p3.data)
+        favList.append(p4.data)
+        
+
+    
 }
-
-
-Promise.all(favNumarr)
-    .then(res => (
-        res.forEach(num =>{
-        const li = document.createElement('li')
-        li.append(num.data)
-        favList.append(li)
-        }
-        )))
+favNums()
